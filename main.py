f1a43b10080436aa6a1fa0286eb5849992b49c98
ade.ristanto@gmail.com <56842597-Ade>
2021-08-16 21:08:11 +0700

    SEQUENTIAL: Eksekusi berurutan

--- main.py
+++ main.py
@@ -5,5 +5,10 @@ print('by Ade')
 print('Tanggal 16 Agustus 2021')
 print('-' * 10)
 
-# PERCABANGAN
+# PERCABANGAN : Eksekusi terpilih
 ingin_cepat = True
+
+if ingin_cepat:
+    print("Jalan lurus aja!!")
+else:
+    print("Jalan lain")
