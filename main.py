206a452e392612bb7a39253e1b85a9077ba7c931
ade.ristanto@gmail.com <56842597-Ade>
2021-08-16 17:06:54 +0700

    # SEQUENTIAL: Eksekusi berurutan

---
+++ main.py
@@ -0,0 +1,9 @@
+# KONSTRUKSI DASAR PYTHON
+# SEQUENTIAL: Eksekusi berurutan
+print('Hello World!')
+print('by Ade')
+print('Tanggal 16 Agustus 2021')
+print('-' * 10)
+
+# PERCABANGAN
+ingin_cepat = True
