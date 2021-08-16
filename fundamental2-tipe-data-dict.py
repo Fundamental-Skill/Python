24e3a28d15629b2f52ab81abcea6b1b7147f190f
ade.ristanto@gmail.com <56842597-Ade>
2021-08-16 23:35:51 +0700

    DICTIONARY

---
+++ fundamental2-tipe-data-dict.py
@@ -0,0 +1,11 @@
+"""
+Tipe data dictionary sekedar menghubungkan antara KEY dan VALUE
+KVP = Key Value Pair
+dictionary = kamus
+"""
+
+kamus_ind_eng = {"anak": "son", "instri": "wife", "ayah": "father", "ibu": "mother"}
+
+print(kamus_ind_eng)
+print(kamus_ind_eng["ayah"])
+print(kamus_ind_eng["ibu"])
